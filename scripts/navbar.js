dd183cf0fdcaf901cf3011fe0b1179de27fa1719
Juan Pablo <juanpablodiaz2518@gmail.com>
2025-12-15 22:48:50 -0300

    script para el desplazarse a diferences secciones desde el navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,29 @@
+document.getElementById("scrollExpLaboral").addEventListener("click", () => {
+  document.getElementById("experienciaLaboral").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("scrollFormAcademica").addEventListener("click", () => {
+  document.getElementById("formacionAcademica").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("scrollHabilidades").addEventListener("click", () => {
+  document.getElementById("competencias").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("scrollProyectos").addEventListener("click", () => {
+  document.getElementById("proyectos").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("scrollContacto").addEventListener("click", () => {
+  document.getElementById("contacto").scrollIntoView({
+    behavior: "smooth"
+  });
+});
